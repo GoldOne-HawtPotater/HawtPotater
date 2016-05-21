ef730081bbc41465a8ee8f82a5d89ed3922cf6f5
Aaron Chau <aaronchauchau@gmail.com>
2016-05-20 23:40:35 -0700

    Made power ups spawn in random x positions.

--- public/js/gameengine.js
+++ public/js/gameengine.js
@@ -38,6 +38,7 @@
         this.potatoCreationQueue = [];
         this.powerUpNextDrop = null;
         this.powerUpDelay = 10000;
+        this.platformPositionData = null;
         this.endGameTime = null;
         this.movingPlatforms = [];
         this.movingPlatformsB2d = [];
@@ -87,7 +88,7 @@
         });    
 
         // Create the platforms for the given map.
-        var platformData = this.createPlatforms({map: 'map0' + data.mapNum});
+        this.createPlatforms({map: 'map0' + data.mapNum});
 
         this.endGameTime = data.endTime;
         this.powerUpNextDrop = data.time + this.powerUpDelay;
@@ -99,7 +100,18 @@
                 setTimeout(pauseLoop, 1);
             } else {
                 that.myGameState = that.gameStates.playing;
-                that.potatoCreationQueue.push({ x: (platformData.minX + platformData.maxX)/2, y: 25, time: data.time, timeToDrop: Date.now() + 5000 });
+                if (that.platformPositionData){
+                    that.potatoCreationQueue.push({ 
+                            x: (that.platformPositionData.minX + that.platformPositionData.maxX)/2, 
+                            y: 25, 
+                            time: data.time, 
+                            timeToDrop: Date.now() + 5000 
+                        });
+                } else {
+                    console.log("Error creating game room.");
+                    that.myGameState = that.gameStates.waiting;
+                    that.endGameTime = Date.now();
+                }
             }
         })();
         // while(Date.now() < data.time);
@@ -265,6 +277,7 @@
             this.players.forEach(function(player) {
                 player.isReady = false;
             }); 
+            that.platformPositionData = null;
             this.createPlatforms({});
             // clear the room  
         }
@@ -278,7 +291,8 @@
     /////////////////////////////////////////////////
     GameEngine.prototype.createPlatforms = function(data) {
         var that = this;
-        var returnData = {};
+        this.platformPositionData = {};
+
         this.platformsB2d.forEach(function (plat, index, array) {
             // that.b2dWorld.DestroyBody(plat);
             plat.GetWorld().DestroyBody(plat);
@@ -344,8 +358,8 @@
 					});
 
                     // Get min x and max x
-                    returnData.minX = colObj.x;
-                    returnData.maxX = colObj.width;
+                    this.platformPositionData.minX = colObj.x;
+                    this.platformPositionData.maxX = colObj.width;
 				}
 
 				//platformBody.SetUserData({
@@ -397,8 +411,6 @@
         } else {
             console.log("TileMaps is undefined. + \nData: " + TileMaps);
         }
-
-        return returnData;
     };
 
     GameEngine.prototype.createListener = function (world) {
@@ -605,7 +617,8 @@
         fixDef.filter.maskBits = this.collisionProfiles.player | this.collisionProfiles.platform | this.collisionProfiles.powerup;
 
 
-        bodyDef.position.x = (Math.floor(Math.random() * (650 - 450 + 1) + 450)) / this.SCALE;
+        // bodyDef.position.x = (Math.floor(Math.random() * (650 - 450 + 1) + 450)) / this.SCALE;
+        bodyDef.position.x = (this.platformPositionData.minX + (Math.floor(Math.random() * this.platformPositionData.maxX))) / this.SCALE;
         bodyDef.position.y = 50 / this.SCALE;
 
         var body = this.b2dWorld.CreateBody(bodyDef);
